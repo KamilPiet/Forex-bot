8a830ef0462c254a8de2a33d6fac1264097499b5
Kamil Pietrusiński <s165435@student.pg.edu.pl>
2022-03-06 13:13:22 +0100

    Test AlphaVantage API Add basic USD/PLN exchange rate information

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import discord
 import os
+from alpha_vantage.foreignexchange import ForeignExchange
 
 client = discord.Client()
 
 
+def get_exchange_rate(from_currency, to_currency):
+    fe = ForeignExchange(key=str(os.getenv('ALPHAVANTAGE_API_KEY')))
+    data, _ = fe.get_currency_exchange_rate(from_currency='USD', to_currency='PLN')
+    return data
+
+
 @client.event
 async def on_ready():
-    print('Bot {0.user}.format(client) started')
+    print('Bot {0.user} '.format(client) + ' started')
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$usdpln'):
+        await message.channel.send(str(get_exchange_rate('USD', 'PLN')))
 
 client.run(os.getenv('TOKEN'))
