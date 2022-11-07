3ad8b6df5437e5b567bd86119692bb619aaaf8ca
Kamil Pietrusiński <s165435@student.pg.edu.pl>
2022-11-07 16:01:15 +0100

    Fix creating bot object - add intents

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ from discord.ext import commands
 #
 #
 # bot = commands.Bot(command_prefix='!', help_command=CustomHelpCommand)
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 auto_flag = False
 
